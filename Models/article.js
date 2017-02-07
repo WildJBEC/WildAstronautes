1512d54c42f723f75cfaf446de68bb5e5de3310a
Elsa Delgado <elsadd@hotmail.fr>
2017-02-07 13:53:19 +0100

    "elsa modif controleur"

--- Models/article.js
+++ Models/article.js
@@ -2,25 +2,24 @@
 class Article {
 
   constructor(json){
-
+    this.id = json.id;
     this.art_theme = json.art_theme.toUpperCase();
     this.date_art = json.date_art;
     this.date_trad = json.date_trad;
     this.author = json.author;
     this.auteur = json.auteur;
-  /*on enleve le tableau pour les images 
-    this.img[]._theme = json.img[]._theme;
-    this.img.spec.img_url = json.img.spec.img_url;
-    this.img.spec.img_taille = json.img.spec.img_taille;
-    this.img.img_alt = json.
-    this.img.img_caption =
-    this.img.img_credit = */
-    //this.img = json.img;
+    this.img_theme_ = json.img_theme_;
+    this.img_Small_url = json.img_Small_url;
+    this.img_Large_url = json.img_Large_url;
+    this.img_taille = json.img_taille;
+    this.img_alt = json.img_alt;
+    this.img_caption = json.img_caption;
+    this.img_credit = json.img_credit;
     this.art_txt = json.art_txt;
     this.art_titre = json.art_titre;
     this.art_liennasa = json.art_liennasa;
   }
-
+  console.log(`id article ${this.id}`);
   extraitarticle () {
     return this.art_text+ " " + this.art_liennasa; // trouver la fonction jquery certain nbre de mots
   }
